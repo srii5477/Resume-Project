f96a647f65d60825da0dd588158b899d28e21868
srii5477 <118789592+srii5477@users.noreply.github.com>
2023-12-21 22:03:13 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,17 @@
+
+document.querySelector(".a").addEventListener("mouseover", () => {
+    let element = document.querySelector(".p1");
+        element.style.display = "flex";
+    // } else {
+    //     element.style.display = "none";
+    // }
+});
+
+document.querySelector(".b").addEventListener("mouseover", () => {
+    let element = document.querySelector(".p2");
+        element.style.display = "flex";
+    
+    // } else {
+    //     element.style.display = "none";
+    // }
+});
